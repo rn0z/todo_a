ec9debb5139f3f17da0c4aa614f873eebebbcbe3
rn0z <manutsawee113@gmail.com>
2019-10-11 22:05:42 +0700

    Test router is work?

--- client/src/Main.js
+++ client/src/Main.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function Main() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+
+      </div>
+      <div className="body">
+        <h1>
+          Test Router
+        </h1>
+      </div>
     </div>
   );
 }
