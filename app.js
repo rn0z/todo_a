897ed142b740e6a9c4e581844837d21298dcc9d0
rn0z <manutsawee113@gmail.com>
2019-10-11 20:10:48 +0700

    Test sending json

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'client', 'build')))
 
 app.get('/api/:whichAPI', (req, res) => {
-
+    res.json({'hello': 'world'})
+    console.log('Sent list of items');
 })
 
 app.get('*', (req, res) => {
