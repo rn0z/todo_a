fa6038d4e67ab108deebca26d0295c45ff9fc339
rn0z <manutsawee113@gmail.com>
2019-10-11 19:58:47 +0700

    GET handle not found

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'client', 'build')))
 
 app.get('*', (req, res) => {
-
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
 })
 
 const port = process.env.PORT | 3000
